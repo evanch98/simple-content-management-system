5217611f42b60f1caf63de1996aed107774eb7f5
Kyaw Thu <evanch98@gmail.com>
2024-10-14 19:31:42 +0800

    replacing fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
 import { ConvexClientProvider } from '../providers/convex-client-provider';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -28,9 +17,7 @@ export default function RootLayout({
   return (
     <ConvexAuthNextjsServerProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={GeistSans.className}>
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
       </html>
