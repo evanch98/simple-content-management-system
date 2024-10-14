292c866e29684d0f7b14ff1efd460024a1a46546
Kyaw Thu <evanch98@gmail.com>
2024-10-14 18:17:19 +0800

    adding a new http action

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,14 @@
 import { httpRouter } from 'convex/server';
 import { auth } from './auth';
+import { publicGet } from './project';
 
 const http = httpRouter();
+
+http.route({
+  path: '/project',
+  method: 'GET',
+  handler: publicGet,
+});
 
 auth.addHttpRoutes(http);
 
