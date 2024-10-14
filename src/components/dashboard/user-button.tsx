d8ad9ed75da448ea10acbab2118cfe879593d52b
Kyaw Thu <evanch98@gmail.com>
2024-10-14 19:31:16 +0800

    adding a new component

---
+++ src/components/dashboard/user-button.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { useAuthActions } from '@convex-dev/auth/react';
+
+interface UserButtonProps {
+  profileUrl?: string;
+  profileName?: string;
+}
+
+export const UserButton = ({
+  profileName = 'SCMS',
+  profileUrl,
+}: UserButtonProps) => {
+  const initials = profileName.substring(0, 2).toUpperCase();
+  const { signOut } = useAuthActions();
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Avatar>
+          <AvatarImage src={profileUrl} />
+          <AvatarFallback>{initials}</AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent
+        className="w-36 p-1"
+        side="bottom"
+      >
+        <Button
+          onClick={() => void signOut()}
+          variant="outline"
+          className="w-full"
+        >
+          Sign out
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
