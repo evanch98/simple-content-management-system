06f2664019383ddaa7736e31c39e08c5c083265a
Kyaw Thu <evanch98@gmail.com>
2024-10-14 18:16:54 +0800

    adding a new endpoint for public access

--- convex/project.ts
+++ convex/project.ts
@@ -1,6 +1,13 @@
 import { v } from 'convex/values';
-import { mutation, query } from './_generated/server';
+import {
+  httpAction,
+  internalQuery,
+  mutation,
+  query,
+} from './_generated/server';
 import { getAuthUserId } from '@convex-dev/auth/server';
+import { Id } from './_generated/dataModel';
+import { internal } from './_generated/api';
 
 export const create = mutation({
   args: {
@@ -40,6 +47,44 @@ export const get = query({
 
     if (project.userId !== userId) {
       throw new Error('Unauthorized');
+    }
+
+    return project;
+  },
+});
+
+export const publicGet = httpAction(async (ctx, request) => {
+  const url = new URL(request.url);
+
+  const projectId =
+    url.searchParams.get('projectId') ??
+    request.headers.get('projectId') ??
+    null;
+
+  if (projectId === null) {
+    return new Response('Did not specify project id as query param or header', {
+      status: 400,
+    });
+  }
+
+  try {
+    const project = await ctx.runQuery(internal.project.fetchProject, {
+      id: projectId as Id<'projects'>,
+    });
+
+    return Response.json(project);
+  } catch (error) {
+    return new Response(`${error}`, { status: 404 });
+  }
+});
+
+export const fetchProject = internalQuery({
+  args: { id: v.id('projects') },
+  handler: async (ctx, args) => {
+    const project = await ctx.db.get(args.id);
+
+    if (!project) {
+      throw new Error('Not found');
     }
 
     return project;
