9f80f08c9fe9b1e09091c9cf1eab4f9691e1600f
Kyaw Thu <evanch98@gmail.com>
2024-10-14 23:39:15 +0800

    initializing the main page with bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,109 @@
 'use client';
 
-import { useAuthActions } from '@convex-dev/auth/react';
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(2, { message: 'Title should be at least 2 characters.' })
+    .max(50),
+});
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-  return <button onClick={() => void signOut()}>Sign out</button>;
+  const router = useRouter();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.project.create);
+
+  const onDialogClose = () => {
+    if (!projects || projects.length === 0) {
+      setDialogOpen(true);
+    } else {
+      setDialogOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!projects || projects.length === 0) {
+      setDialogOpen(true);
+    } else {
+      router.replace(`/${projects[0]?._id}`);
+    }
+  }, [projects]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const newProject = await createProject({ title: values.title });
+    router.replace(`/${newProject}`);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={dialogOpen}
+        onOpenChange={() => onDialogClose()}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create a new project</DialogTitle>
+            <DialogDescription>Create a project to continue.</DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Simple CMS"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          <DialogFooter>
+            <Button onClick={form.handleSubmit(onSubmit)}>Create</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
