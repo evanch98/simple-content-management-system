b72734731b70cbb785fbcf204698de87fc86056b
Kyaw Thu <evanch98@gmail.com>
2024-10-15 15:11:14 +0800

    updating the style

--- src/app/(dashboard)/[projectId]/layout.tsx
+++ src/app/(dashboard)/[projectId]/layout.tsx
@@ -9,7 +9,7 @@ interface DashboardLayoutProps {
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <section className="mx-auto w-full max-w-screen-2xl px-4 lg:px-8">
+    <section className="mx-auto h-full w-full max-w-screen-2xl px-4 lg:px-8">
       <Navbar />
       {children}
     </section>
