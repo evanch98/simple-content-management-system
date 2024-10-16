aa77fbf498a5d9857396f9e6fb99109dfc5c1c26
Kyaw Thu <evanch98@gmail.com>
2024-10-16 12:47:39 +0800

    adding a new modal for creating pages

---
+++ src/components/modals/page-create-modal.tsx
@@ -0,0 +1,116 @@
+'use client';
+
+import { useMutation } from 'convex/react';
+import { useParams } from 'next/navigation';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { api } from '../../../convex/_generated/api';
+import { useState } from 'react';
+import { usePageCreateModal } from '@/store/use-page-create-modal';
+import { Id } from '../../../convex/_generated/dataModel';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(2, { message: 'Title should be at least 2 characters.' })
+    .max(50),
+});
+
+export const PageCreateModal = () => {
+  const params = useParams();
+  const { isOpen, onClose } = usePageCreateModal();
+
+  const createPage = useMutation(api.page.create);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setIsLoading(true);
+      await createPage({
+        title: values.title,
+        projectId: params.projectId as Id<'projects'>,
+      });
+      onClose();
+    } catch (error) {
+      // TODO: add toast
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={onClose}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a new page</DialogTitle>
+          <DialogDescription>Ready to create a page?</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Home page"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            disabled={isLoading}
+            onClick={onClose}
+            variant="outline"
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={isLoading}
+            onClick={form.handleSubmit(onSubmit)}
+          >
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
