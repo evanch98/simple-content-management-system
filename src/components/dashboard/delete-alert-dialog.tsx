3002b3cee93cad4b01ffb9a0b416e19fa5156c61
Kyaw Thu <evanch98@gmail.com>
2024-10-17 13:20:46 +0800

    adding a new component

---
+++ src/components/dashboard/delete-alert-dialog.tsx
@@ -0,0 +1,39 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+
+interface DeleteAlertDialogProps {
+  children: React.ReactNode;
+  description: string;
+  onConfirm: () => void;
+}
+
+export const DeleteAlertDialog = ({
+  children,
+  description,
+  onConfirm,
+}: DeleteAlertDialogProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Confirm</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
