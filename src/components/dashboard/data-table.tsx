f4ae24e871f4cb6cf222e2ba2d2c12699e141f50
Kyaw Thu <evanch98@gmail.com>
2024-10-17 11:52:15 +0800

    updating the empty datatable text

--- src/components/dashboard/data-table.tsx
+++ src/components/dashboard/data-table.tsx
@@ -89,7 +89,7 @@ export function DataTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  No pages found.
                 </TableCell>
               </TableRow>
             )}
