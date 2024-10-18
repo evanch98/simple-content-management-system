e4c759038a5f49d6e9de89947f2c8378377640c2
Kyaw Thu <evanch98@gmail.com>
2024-10-18 18:25:17 +0800

    updating the store

--- src/store/use-section-edit-modal.tsx
+++ src/store/use-section-edit-modal.tsx
@@ -1,16 +1,19 @@
 import { create } from 'zustand';
+import { Id } from '../../convex/_generated/dataModel';
 
 interface SectionEditModalStore {
   name: string;
+  id: Id<'sections'> | undefined;
   isOpen: boolean;
   // eslint-disable-next-line no-unused-vars
-  onOpen: (title: string) => void;
+  onOpen: (title: string, id: Id<'sections'>) => void;
   onClose: () => void;
 }
 
 export const useSectionEditModal = create<SectionEditModalStore>((set) => ({
   name: '',
+  id: undefined,
   isOpen: false,
-  onOpen: (name: string) => set({ isOpen: true, name }),
-  onClose: () => set({ isOpen: false, name: '' }),
+  onOpen: (name: string, id: Id<'sections'>) => set({ isOpen: true, name, id }),
+  onClose: () => set({ isOpen: false, name: '', id: undefined }),
 }));
