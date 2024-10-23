8c41159f1e29fcee1d059f9fdf9ffb3ff6dad6db
Kyaw Thu <evanch98@gmail.com>
2024-10-23 18:35:58 +0800

    adding image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['geist'],
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'enchanted-newt-444.convex.cloud',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
